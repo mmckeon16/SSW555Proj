f11a7243e4fd8fcbf0b1cb703cdf52e91f942a61
rachel stern <40131531+renaissancepaintings@users.noreply.github.com>
2018-06-29 23:50:55 -0400

    Update us09.py
    adding my refactoring

--- src/us09.py
+++ src/us09.py
@@ -4,7 +4,7 @@ from datetime import datetime
 
 def birthbeforedeath(childsname, childID, childsbirthday, parentsdeath, deathBool):
 	
-	if ((rs_stories.form_d(childsbirthday, parentsdeath) == 2) and (deathBool == False)):
+	if ((datetime.strptime(childsbirthday, '%d %b %Y')) > (datetime.strptime(parentsdeath, '%d %b %Y')) and (deathBool == False)):
 		date_birth = datetime.strptime(childsbirthday, '%d %b %Y')
 		parentsdeath = datetime_object = datetime.strptime(parentsdeath, '%d %b %Y')
 		dif_time = ((childsbirthday-parentsdeath).days/365.25) * 12
@@ -13,7 +13,7 @@ def birthbeforedeath(childsname, childID, childsbirthday, parentsdeath, deathBoo
 			f.write("Error US09: Birthdate of child " + childsname + " (" + childID + ") is >9 months after father's death .\n")
 			f.close()
 	
-	elif (rs_stories.form_d(childsbirthday, parentsdeath) == 2):
+	elif ((datetime.strptime(childsbirthday, '%d %b %Y')) > (datetime.strptime(parentsdeath, '%d %b %Y'))):
 		f=open("../test/acceptanceTestOutput.txt","a+")
 		f.write("Error US09: Birthdate of child " + childsname + " (" + childID + ") is after mother's death.\n")
 		f.close()
