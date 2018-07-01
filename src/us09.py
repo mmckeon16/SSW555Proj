9601903934bbe436e7303f9eadf96931ab89ef30
rachel stern <40131531+renaissancepaintings@users.noreply.github.com>
2018-07-01 01:18:08 -0400

    refactored for homework 6

--- src/us09.py
+++ src/us09.py
@@ -1,19 +1,18 @@
 import unittest
-import rs_stories
 from datetime import datetime
 
-def birthbeforedeath(childsname, childID, childsbirthday, parentsdeath, deathBool):
+def birthbeforedeath(childsname, childsID, childsbirthday, parentsdeathdate, deathBool):
 	
-	if ((datetime.strptime(childsbirthday, '%d %b %Y')) > (datetime.strptime(parentsdeath, '%d %b %Y')) and (deathBool == False)):
+	if ((datetime.strptime(childsbirthday, '%d %b %Y')) > (datetime.strptime(parentsdeathdate, '%d %b %Y')) and (deathBool == False)):
 		date_birth = datetime.strptime(childsbirthday, '%d %b %Y')
-		parentsdeath = datetime_object = datetime.strptime(parentsdeath, '%d %b %Y')
-		dif_time = ((childsbirthday-parentsdeath).days/365.25) * 12
+		parentsdeath = datetime_object = datetime.strptime(parentsdeathdate, '%d %b %Y')
+		dif_time = ((childsbirthday-parentsdeathdate).days/365.25) * 12
 		if (dif_time > 9):
 			f=open("../test/acceptanceTestOutput.txt","a+")
-			f.write("Error US09: Birthdate of child " + childsname + " (" + childID + ") is >9 months after father's death .\n")
+			f.write("Error US09: Birthdate of child " + childsname + " (" + childsID + ") is >9 months after father's death .\n")
 			f.close()
 	
-	elif ((datetime.strptime(childsbirthday, '%d %b %Y')) > (datetime.strptime(parentsdeath, '%d %b %Y'))):
+	elif ((datetime.strptime(childsbirthday, '%d %b %Y')) > (datetime.strptime(parentsdeathdate, '%d %b %Y'))):
 		f=open("../test/acceptanceTestOutput.txt","a+")
-		f.write("Error US09: Birthdate of child " + childsname + " (" + childID + ") is after mother's death.\n")
+		f.write("Error US09: Birthdate of child " + childsname + " (" + childsID + ") is after mother's death.\n")
 		f.close()
