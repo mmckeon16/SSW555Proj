c59c5c4834f8474f881524723ab24d002fbd7365
Ruthy Levi <rlevi@stevens.edu>
2018-07-08 00:24:37 -0400

    Update noBigamy.py

--- src/noBigamy.py
+++ src/noBigamy.py
@@ -1,6 +1,5 @@
 import unittest 
 
-
 def safe_open(file,mode):
     try:
         return open(file, mode)
@@ -9,28 +8,43 @@ def safe_open(file,mode):
 
 """Marriage should not occur during marriage to another spouse"""
 def checkBigamy(fam,ind):
-    #if the value of husb or the value of husb in one family is equal to the value of husb or the value of wife of another fam
-    newDictionary = {}
-    count = 0
     for f in fam:
-        hus_id = fam[f]['HUSB']
-        wife_id = fam[f]['WIFE']
+        if 'HUSB' in fam[f]:
+            hus_id = fam[f]['HUSB']
+            if 'WIFE' in fam[f]:
+                wife_id = fam[f]['WIFE']
 
         wife_count = 0
         husb_count = 0
+
         #then do another for loop
         for f in fam:
-            hus_id2 = fam[f]['HUSB']
-            wife_id2 = fam[f]['WIFE']
-            #compre the 2 husbands, if they are the same, add one to the husb_count
-            #compare the 2 wives, if they are the same, add one to the wife_count
-        #check if husb_count or wife_count is greater than 1, if so, save the error. If not, continue
-        
-fam = {'F23': 
+            if 'HUSB' in fam[f]:
+                hus_id2 = fam[f]['HUSB']
+                if hus_id == hus_id2:
+                    husb_count += 1
+                if 'WIFE' in fam[f]:
+                    wife_id2 = fam[f]['WIFE']
+                    if wife_id == wife_id2:
+                        wife_count += 1
+            else:
+              continue
+            if husb_count > 1: #if bigamy occurs, remove both instances of a spouse
+                print("US11 ERROR: Marriage should not occur during marriage to another spouse")
+                popped(hus_id)
+                break
+            if wife_count > 1:
+                print("US11 ERROR: Marriage should not occur during marriage to another spouse")
+                popped(wife_id)
+                break
+
+def popped(any_list):
+    fam.pop(any_list, None)
+    ind.pop(any_list, None)
+    
+
+fam = {'F23': #no bigamy
   {'fam': 'F23', 'MARR': '14 FEB 1980', 'HUSB': 'I01', 'WIFE': 'I07', 'CHIL': ['I19', 'I26', 'I30']},
-   'F16': {'fam': 'F16', 'MARR': '12 DEC 2007'}}
-fam2 = {'F24': 
-  {'fam': 'F24', 'MARR': '14 FEB 1980', 'HUSB': 'I01', 'WIFE': 'I10', 'CHIL': ['I19', 'I26', 'I30']},
    'F16': {'fam': 'F16', 'MARR': '12 DEC 2007'}}
 ind = {'I01': {'id': 'I01', 'name': 'Joe /Smith/', 'BIRT': '15 JUL 1960', 'sex': 'M', 'family': 'F23', 'DEAT': '31 DEC 2013'},
  'I07': {'id': 'I07', 'name': 'Jennifer /Smith/', 'BIRT': '23 SEP 1960', 'sex': 'F', 'family': 'F23'},
@@ -38,37 +52,41 @@ ind = {'I01': {'id': 'I01', 'name': 'Joe /Smith/', 'BIRT': '15 JUL 1960', 'sex':
   'I26': {'id': 'I26', 'name': 'Jane /Smith/', 'BIRT': '13 FEB 1981', 'sex': 'F', 'family': 'F23'},
   'I30': {'id': 'I30', 'name': 'Mary /Test/', 'BIRT': '13 FEB 1981', 'sex': 'F', 'family': 'F23'},
   'I32': {'id': 'I32', 'name': 'Nick /Tary/', 'BIRT': '13 FEB 1981', 'sex': 'M', 'family': 'F23'},
-  'I44': {'id': 'I44', 'name': 'Cersi /Lanister/', 'BIRT': '13 FEB 1981', 'sex': 'F', 'family': 'F24'}}
-ind2 = {'I01': {'id': 'I01', 'name': 'Joe /Smith/', 'BIRT': '15 JUL 1960', 'sex': 'M', 'family': 'F24', 'DEAT': '31 DEC 2013'},
- 'I10': {'id': 'I10', 'name': 'Julie /Brown/', 'BIRT': '23 SEP 1960', 'sex': 'F', 'family': 'F24'},
- 'I19': {'id': 'I19', 'name': 'Whitney /Brown/', 'BIRT': '13 FEB 1981', 'sex': 'M', 'family': 'F24'},
-  'I26': {'id': 'I26', 'name': 'Laura /Brown/', 'BIRT': '13 FEB 1981', 'sex': 'F', 'family': 'F24'},
-  'I30': {'id': 'I30', 'name': 'Julia /Test/', 'BIRT': '13 FEB 1981', 'sex': 'F', 'family': 'F24'},
-  'I32': {'id': 'I32', 'name': 'Eliza /Tary/', 'BIRT': '13 FEB 1981', 'sex': 'M', 'family': 'F24'},
-  'I44': {'id': 'I44', 'name': 'Alex /Lanister/', 'BIRT': '13 FEB 1981', 'sex': 'F', 'family': 'F24'}}
+  'I44': {'id': 'I44', 'name': 'Cersi /Lanister/', 'BIRT': '13 FEB 1981', 'sex': 'F', 'family': 'F23'}}
 
-fam3 = {'F23': #dad and son have same name and bday
-  {'fam': 'F23', 'MARR': '14 FEB 1980', 'WIFE': 'I07', 'CHIL': ['I19', 'I26', 'I30']},
-   'F16': {'fam': 'F16', 'MARR': '12 DEC 2007'}}
-ind3 = {'I07': {'id': 'I07', 'name': 'Jennifer /Smith/', 'BIRT': '23 SEP 1960', 'sex': 'F', 'family': 'F23'},
- 'I19': {'id': 'I19', 'name': 'Joe /Smith/', 'BIRT': '15 JUL 1960', 'sex': 'M', 'family': 'F23'},
+fam2 = {'F23': #bigamy (same husband)
+  {'fam': 'F23', 'MARR': '14 FEB 1980', 'HUSB': 'I01', 'WIFE': 'I07', 'CHIL': ['I19', 'I26', 'I30']},
+   'F16': {'fam': 'F16', 'MARR': '12 DEC 2007', 'HUSB': 'I01'}}
+ind2 = {'I01': {'id': 'I01', 'name': 'Joe /Smith/', 'BIRT': '15 JUL 1960', 'sex': 'M', 'family': 'F23', 'DEAT': '31 DEC 2013'},
+ 'I07': {'id': 'I07', 'name': 'Jennifer /Smith/', 'BIRT': '23 SEP 1960', 'sex': 'F', 'family': 'F23'},
+ 'I19': {'id': 'I19', 'name': 'Dick /Smith/', 'BIRT': '13 FEB 1981', 'sex': 'M', 'family': 'F23'},
   'I26': {'id': 'I26', 'name': 'Jane /Smith/', 'BIRT': '13 FEB 1981', 'sex': 'F', 'family': 'F23'},
   'I30': {'id': 'I30', 'name': 'Mary /Test/', 'BIRT': '13 FEB 1981', 'sex': 'F', 'family': 'F23'},
   'I32': {'id': 'I32', 'name': 'Nick /Tary/', 'BIRT': '13 FEB 1981', 'sex': 'M', 'family': 'F23'},
   'I44': {'id': 'I44', 'name': 'Cersi /Lanister/', 'BIRT': '13 FEB 1981', 'sex': 'F', 'family': 'F23'}}
 
+fam3 = {'F23': #bigamy (same wife)
+  {'fam': 'F23', 'MARR': '14 FEB 1980', 'HUSB': 'I01', 'WIFE': 'I07', 'CHIL': ['I19', 'I26', 'I30']},
+   'F16': {'fam': 'F16', 'MARR': '12 DEC 2007','WIFE': 'I07'}}
+ind3 = {'I01': {'id': 'I01', 'name': 'Joe /Smith/', 'BIRT': '15 JUL 1960', 'sex': 'M', 'family': 'F23', 'DEAT': '31 DEC 2013'},
+ 'I07': {'id': 'I07', 'name': 'Jennifer /Smith/', 'BIRT': '23 SEP 1960', 'sex': 'F', 'family': 'F23'},
+ 'I19': {'id': 'I19', 'name': 'Dick /Smith/', 'BIRT': '13 FEB 1981', 'sex': 'M', 'family': 'F23'},
+  'I26': {'id': 'I26', 'name': 'Jane /Smith/', 'BIRT': '13 FEB 1981', 'sex': 'F', 'family': 'F23'},
+  'I30': {'id': 'I30', 'name': 'Mary /Test/', 'BIRT': '13 FEB 1981', 'sex': 'F', 'family': 'F23'},
+  'I32': {'id': 'I32', 'name': 'Nick /Tary/', 'BIRT': '13 FEB 1981', 'sex': 'M', 'family': 'F23'},
+  'I44': {'id': 'I44', 'name': 'Cersi /Lanister/', 'BIRT': '13 FEB 1981', 'sex': 'F', 'family': 'F23'}}
 
 class MyTest(unittest.TestCase):
   def test(self):
-      checkIndividual(fam, ind)
+      checkBigamy(fam, ind)
       self.assertTrue(('I01' in ind))
       self.assertTrue(('I01' == fam['F23']['HUSB']))
-      checkIndividual(fam2, ind2)
+      checkBigamy(fam2, ind2)
       self.assertTrue(('I01' in ind2))
       self.assertTrue(('I01' in fam2['F23']['HUSB']))
-      checkIndividual(fam3, ind3)
-      self.assertFalse(('I01' in ind3))
-      self.assertFalse(('HUSB' in fam3['F23']))
+      checkBigamy(fam3, ind3)
+      self.assertTrue(('I07' in ind3))
+      self.assertTrue(('WIFE' in fam3['F23']))
 
 
 def main():
