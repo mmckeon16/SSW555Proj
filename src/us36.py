742c36109113e8744f33b14464aeba37701446f1
jadames1 <36338374+jadames1@users.noreply.github.com>
2018-07-19 18:21:44 -0400

    Add files via upload

---
+++ src/us36.py
@@ -0,0 +1,18 @@
+    def us36_print_recent_deaths(self):
+        """" US36 Print deaths in the last 30 days in pretty table
+        """
+        people = self.individuals
+        table = PrettyTable(["ID", "Name", "Deathdate"])
+
+        for person_id in people:
+            person = self.individuals[person_id]
+            recent_date = self._current_time - timedelta(days=30)
+
+            if person.get_death_date() is None:
+                pass
+            if person.get_death_date() is not None:
+                if recent_date < person.get_death_date() and person.get_death_date() < self._current_time:
+                    table.add_row([person.get_person_id(), person.get_name(), person.get_death_date()])
+
+        print("Recent Deaths")
+        print(table)
