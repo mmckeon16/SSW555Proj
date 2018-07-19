1e2997b659b7659819d33b5add43c0ba435ba8b6
jadames1 <36338374+jadames1@users.noreply.github.com>
2018-07-19 18:21:18 -0400

    Add files via upload

---
+++ src/us35.py
@@ -0,0 +1,17 @@
+    def us35_print_recent_births(self):
+        """" US35 Print births in the last 30 days in pretty table
+        """
+        people = self.individuals
+        table = PrettyTable(["ID", "Name", "Birthdate"])
+
+        for person_id in people:
+            person = self.individuals[person_id]
+            recent_date = self._current_time - timedelta(days=30)
+            if person.get_birth_date() is None:
+                pass
+            if person.get_birth_date() is not None:
+                if recent_date < person.get_birth_date() and person.get_birth_date() < self._current_time:
+                    table.add_row([person.get_person_id(), person.get_name(), person.get_birth_date()])
+
+        print("Recent Births")
+        print(table)
