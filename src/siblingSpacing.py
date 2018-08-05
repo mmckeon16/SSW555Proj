d552eea57539dd1e735aff086fd7b88fd117ffeb
Ruthy Levi <rlevi@stevens.edu>
2018-08-05 16:31:44 -0400

    Update siblingSpacing.py

--- src/siblingSpacing.py
+++ src/siblingSpacing.py
@@ -13,7 +13,13 @@ from datetime import datetime
 chil_list = []
 month_values = {"jan":1, "feb":2, "mar":3, "apr":4, "may":5, "jun":6, "jul":7, "aug":8, "sep":9, "oct":10, "nov":11, "dec":12}
 
-def siblingSpacing(fam, ind): 
+def safe_open(file,mode):
+    try:
+        return open(file, mode)
+    except IOError:
+        raise IOError("Can't open '{}' for '{}'".format(file, mode))
+        
+def siblingSpacing(fam, ind, file): 
     result = True 
     for f in fam:
         if "CHIL" in fam[f]: #if person is a child
@@ -43,7 +49,6 @@ def getDay(born):
   born = datetime.strptime(born, '%d %b %Y')
   return born.day
 
-
 fam = {'F23': #birth date of Dick Smith and Jane Smith more than 8 months apart 
   {'fam': 'F23', 'MARR': '14 FEB 1980', 'HUSB': 'I01', 'WIFE': 'I07', 'CHIL': ['I19', 'I26', 'I30']},
    'F16': {'fam': 'F16', 'MARR': '12 DEC 2007'}}
@@ -54,4 +59,36 @@ ind = {'I01': {'id': 'I01', 'name': 'Joe /Smith/', 'BIRT': '15 JUL 1960', 'sex':
   'I30': {'id': 'I30', 'name': 'Mary /Test/', 'BIRT': '13 FEB 1987', 'sex': 'F', 'family': 'F23'},
 }
 
-siblingSpacing(fam,ind)
+fam2 = {'F23': #birth date of Dick Smith and Jane Smith less than 2 days apart
+  {'fam': 'F23', 'MARR': '14 FEB 1980', 'HUSB': 'I01', 'WIFE': 'I07', 'CHIL': ['I19', 'I26', 'I30']},
+   'F16': {'fam': 'F16', 'MARR': '12 DEC 2007'}}
+ind2 = {'I01': {'id': 'I01', 'name': 'Joe /Smith/', 'BIRT': '15 JUL 1960', 'sex': 'M', 'family': 'F23', 'DEAT': '31 DEC 2013'},
+ 'I07': {'id': 'I07', 'name': 'Jennifer /Smith/', 'BIRT': '23 SEP 1960', 'sex': 'F', 'family': 'F23'},
+ 'I19': {'id': 'I19', 'name': 'Dick /Smith/', 'BIRT': '13 FEB 1981', 'sex': 'M', 'family': 'F23'},
+  'I26': {'id': 'I26', 'name': 'Jane /Smith/', 'BIRT': '13 FEB 1981', 'sex': 'F', 'family': 'F23'},
+  'I30': {'id': 'I30', 'name': 'Mary /Test/', 'BIRT': '13 FEB 1987', 'sex': 'F', 'family': 'F23'},
+  }
+
+fam3 = {'F23': #birth date of Dick Smith and Jane Smith less than 8 months apart
+  {'fam': 'F23', 'MARR': '14 FEB 1980', 'HUSB': 'I01', 'WIFE': 'I07', 'CHIL': ['I19', 'I26', 'I30']},
+   'F16': {'fam': 'F16', 'MARR': '12 DEC 2007'}}
+ind3 = {'I01': {'id': 'I01', 'name': 'Joe /Smith/', 'BIRT': '15 JUL 1960', 'sex': 'M', 'family': 'F23', 'DEAT': '31 DEC 2013'},
+ 'I07': {'id': 'I07', 'name': 'Jennifer /Smith/', 'BIRT': '23 SEP 1960', 'sex': 'F', 'family': 'F23'},
+ 'I19': {'id': 'I19', 'name': 'Dick /Smith/', 'BIRT': '13 FEB 1981', 'sex': 'M', 'family': 'F23'},
+  'I26': {'id': 'I26', 'name': 'Jane /Smith/', 'BIRT': '13 APR 1981', 'sex': 'F', 'family': 'F23'},
+  'I30': {'id': 'I30', 'name': 'Mary /Test/', 'BIRT': '13 FEB 1987', 'sex': 'F', 'family': 'F23'},
+  }
+
+class MyTest(unittest.TestCase):
+  def test(self):
+      f= open("../test/ruthyOutput.txt","a+")
+      self.assertFalse(siblingSpacing(fam, ind, f))
+      self.assertFalse(siblingSpacing(fam2, ind2, f))
+      self.assertFalse(siblingSpacing(fam3, ind3, f))
+      f.close()
+
+def main():
+  safe_open("acceptanceTestOutput.txt", 'a+')
+
+if __name__ == "__main__":
+    unittest.main()
