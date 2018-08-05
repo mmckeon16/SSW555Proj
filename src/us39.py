8b72faaceb60ceb04b494d4ce3cf5dea9689bb87
jadames1 <36338374+jadames1@users.noreply.github.com>
2018-08-05 16:14:45 -0400

    Update us39.py

--- src/us39.py
+++ src/us39.py
@@ -1,44 +1,68 @@
 import unittest
-from datetime import date, timedelta
-import datetime
-def IsAlive(ID,IndList):
-    for person_id in ind:
-        if ind[0] == ID:
-            if ind[4][10:] == "Alive":
-                return True
-            else:
-                return False
-def ListUpcomingAnn(FamList,IndList,wr):
-     """"US39
-        Prints anniversaries within the next 30 days
+import PrettyTable
+import datetime, timedelta
+
+def get_is_alive(individual):
+    if "DEAT" in individual:
+        death = datetime.strptime(individual["DEAT"], '%d %b %Y')
+        return False
+    else:
+        return True
+
+
+def get_birth_date(individual):
+    if "BIRT" in individual:
+        birth = datetime.strptime(individual["BIRT"], '%d %b %Y')
+        return birth
+    else:
+        return 0
+    
+def us_38_print_upcoming_birthdays(individuals, file): #why did you have so many inputs in the method?
+        """"US38
+        Prints birthdays within the next 30 days
         """
-        people = self.individuals
-        table = PrettyTable(["ID", "Name", "Anniversaries"])
-    count= 0
-    wr.write("\n\nUS39 - List Upcoming Anniversaries")
-    result =[]
-    for daterange in range(30):
-        result = result + [(datetime.datetime.now().date() + timedelta(daterange))]
-    for famID in ind[key]['family']:        
-        if Fam[-1][:3] == "DIV":
-            pass
-        else: 
-            if IsAlive(Fam[1][5:],IndList) and IsAlive(Fam[2][5:],IndList):
-                MarDate= datetime.datetime.strptime(Fam[-1][10:], '%Y-%m-%d').date()
-                today= date.today() 
-                AnnDate= datetime.date(today.year, MarDate.month, MarDate.day)
-                if 0 <= (AnnDate - today).days <= 30:
-                    count= count+1
-                    output= "\nThe Anniversary for family " + Fam[0] + " is in " + str(AnnDate)
-                    wr.write(str(output))
-    if count == 0:
-        wr.write("\nNo Upcoming Anniversary")
-        
-        
+        people = individuals
+        table = PrettyTable(["ID", "Name", "Birthday"])
+
+        result = False
+
+        for person_id in people:
+            individual = individuals[person_id]
+            #also, no self reference and use people if you made that assignment
+            individual = people[person_id] 
+            today = datetime.today()
+            #individual_birthday = individual.get_birth_date() 
+            #Again, like last time, we do not have this function, you need to make this function if you want to use it
+            #I added the function for you so you can copy it into files if you need to use it
+            individual_birthday = get_birth_date(individual)
+            if individual_birthday is not 0 and get_is_alive(individual):
+                individual_current_birthday = datetime(today.year, individual_birthday.month, individual_birthday.day)
+                print(str(individual_current_birthday))
+                if 0 <= (abs(individual_current_birthday - today)).days <= 30:
+                    #table.add_row([individual.get_person_id(), individual.get_name(), individual_birthday])
+                    #the id is just the individual["id"].
+                    #the name is individual["name"]
+                    #so the real result should look like:
+                    table.add_row([individual["id"], individual["name"], individual_birthday])
+                    result = True
+        file.write("Upcoming Birthdays\n")
+        file.write(str(table) + "\n") #you cannot just print the stories, you have to write them to our test file
+        return result
+
+ind = {'I01': {'id': 'I01', 'name': 'Joe /Smith/', 'BIRT': '15 JUL 1960', 'sex': 'M', 'family': 'F23', 'DEAT': '31 DEC 2013'},
+ 'I07': {'id': 'I07', 'name': 'Jennifer /Smith/', 'BIRT': '23 JUL 1960', 'sex': 'F', 'family': 'F23'},
+ 'I19': {'id': 'I19', 'name': 'Dick /Smith/', 'BIRT': '13 FEB 1981', 'sex': 'M', 'family': 'F23'},
+  'I26': {'id': 'I26', 'name': 'Jane /Smith/', 'BIRT': '13 FEB 1982', 'sex': 'F', 'family': 'F23'},
+  'I30': {'id': 'I30', 'name': 'Mary /Test/', 'BIRT': '13 FEB 1987', 'sex': 'F', 'family': 'F23'},
+}
+
 class MyTest(unittest.TestCase):
     def test(self): 
+
+        #you are trying to check if something is true but the function is not returning anything
         f=open("../test/jeneleeOutput.txt","a+")
-        self.assertTrue(us38_print_upcoming_birthdays(ind,f))
+        self.assertTrue(us_38_print_upcoming_birthdays(ind,f))
+        #you did not copy over the ind dictionary...
         f.close()
         
 if __name__ == '__main__':
